b37e910789726de2296084e000105e82933cfb6d
김혜리 (KIM HYE RI) 대리 (인실리코AI 1팀) <hrkim2@seegene.com>
2022-01-13 10:08:05 +0900

    ~class에서 state 사용 / branch test

--- src/App.js
+++ src/App.js
@@ -5,25 +5,34 @@ function App() {
   return (
     <div className="App">
       <h1>Hello world!</h1>
-      <FuncComp/>
-      <ClassComp/>
+      <FuncComp initNumber={2}/>
+      <ClassComp initNumber={2}/>
     </div>
   );
 }
 
-function FuncComp(){
+function FuncComp(props){
   return (
     <div className="container">
       <h2>function style component</h2>
+        <p>Number : {props.initNumber}</p>
     </div>
   );
 }
 
 class ClassComp extends React.Component {
+  state = {
+    number: this.props.initNumber
+  }
   render(){
     return (
       <div className="container">
         <h2>class style component</h2>
+        <p>Number : {this.state.number}</p>
+        <input type="button" value="random" onClick={function(){
+          this.setState({number: Math.round(Math.random()*100000)})
+        }.bind(this)}
+        />
       </div>
     );
   }
